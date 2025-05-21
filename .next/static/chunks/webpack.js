db295cd6e37d46d8af1fcef5b36e4af8e15941aa
Home7869 <pgwaala4u@gmail.com>
2025-05-21 10:24:19 +0530

    Fix Next.js Configuration Issues

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "58e1a1ab39cf372e"; }
+/******/ 		__webpack_require__.h = function() { return "4dd502b881ac7d25"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
